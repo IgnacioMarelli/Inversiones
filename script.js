c2e2fd53859c4d3a49a0f0ab4d4a5a0c6b1c7020
IgnacioMarelli <ignacio.marelli@hotmail.com>
2025-09-30 18:56:27 -0300

    s

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         { nombre: 'QQQ', id: 'QQQ', precioDeCompra: 553.619, cantidadComprada: 0.16256594, tipo: 'stock' },
         { nombre: 'SPY 500', id: 'SPY', precioDeCompra: 621.616, cantidadComprada: 0.14478885, tipo: 'stock' },
         { nombre: 'DGRO', id: 'DGRO', precioDeCompra: 66.273, cantidadComprada: 0.754474, tipo: 'stock' },
-        { nombre: 'PYPL', id: 'PYPL', precioDeCompra: 72.508, cantidadComprada: 0.689681, tipo: 'stock' },
+        { nombre: 'PYPL', id: 'PYPL', precioDeCompra: 69.755, cantidadComprada: 1.433582, tipo: 'stock' },
         { nombre: 'XLV', id: 'XLV', precioDeCompra: 141.749, cantidadComprada: 0.352769, tipo: 'stock' },
         { nombre: 'ADBE', id: 'ADBE', precioDeCompra: 364.07, cantidadComprada: 0.1374, tipo: 'stock' },
         { nombre: 'AAPL', id: 'AAPL', precioDeCompra: 229.36, cantidadComprada: 0.0436, tipo: 'stock' }
